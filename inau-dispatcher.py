b9b4976fc98a9f5c61fdc5b759268c850e3c747f
Alessio Igor Bogani <alessio.bogani@elettra.eu>
2024-11-18 11:40:20 +0100

    Remove old comments and allow building only on default_branch

--- inau-dispatcher.py
+++ inau-dispatcher.py
@@ -15,16 +15,12 @@ import logging
 import logging.handlers
 import argparse
 import datetime
-#import time
 import subprocess
 import paramiko
 import hashlib
 import shutil
-#import requests
-#import urllib.parse
 from smtplib import SMTP
 from email.mime.text import MIMEText
-#from distutils.version import StrictVersion
 
 from lib import db
 
@@ -32,8 +28,6 @@ Session = sessionmaker()
 
 allbuilders = {}
 users = {}
-
-# TODO Build a repository against ALL platforms and avoid emails entirely
 
 def __sendEmail(to_addrs, subject, body):
     if to_addrs:
@@ -61,25 +55,25 @@ def sendEmailAdmins(subject, body):
             to_addrs.add(user.name + "@" + args.smtpdomain)
     __sendEmail(to_addrs, subject, body)
 
-# TODO Move follow classes to a separate file
 class Die:
     pass
 
 class Update:
-    def __init__(self, repository_name, repository_url, build_tag):
+    def __init__(self, repository_name, repository_url, build_tag, default_branch):
         self.repository_name = repository_name
         self.repository_url = repository_url
         self.build_tag = build_tag
+        self.default_branch = default_branch
 
 class Build(Update):
-    def __init__(self, repository_name, repository_url, build_tag):
-        Update.__init__(self, repository_name, repository_url, build_tag)
+    def __init__(self, repository_name, repository_url, build_tag, default_branch):
+        Update.__init__(self, repository_name, repository_url, build_tag, default_branch)
         self.status = ''
         self.output = ''
 
 class Store(Build):
-    def __init__(self, repository_name, repository_url, build_tag, repository_id, repository_type, emails):
-        Build.__init__(self, repository_name, repository_url, build_tag)
+    def __init__(self, repository_name, repository_url, build_tag, repository_id, repository_type, emails, default_branch):
+        Build.__init__(self, repository_name, repository_url, build_tag, default_branch)
         self.repository_id = repository_id
         self.repository_type = repository_type
         self.emails = emails
@@ -101,9 +95,6 @@ class Builder:
         logger.info("[" + self.name + "] Checkouting " + job.build_tag + " from " + job.repository_url + "...")
         builddir = self.platdir + "/" + job.repository_name
         buildcmd = ""
-        # Wait a second before start to update git repository to avoid:
-        # Command '['git -C /scratch/build/repositories//8/cs/etc/browser/booster reset --hard refs/tags/1.0.34']' returned non-zero exit status 128.
-        # time.sleep(1)
         if not os.path.isdir(self.platdir):
             os.mkdir(self.platdir)
 
@@ -116,9 +107,8 @@ class Builder:
         if not os.path.isdir(builddir):
             buildcmd += " && git clone --recurse-submodule " + job.repository_url + " " + builddir
         buildcmd += " && git -C " + builddir + " remote update"
-        buildcmd += " && git -C " + builddir + " fetch --tags"
-#       subprocess.run(["git -C " + builddir + " pull"], shell=True, check=True)
-#       subprocess.run(['git branch --no-color --contains ' + job.build_tag + ' master | grep \<master\>', 
+        buildcmd += " && git -C " + builddir + " pull --tags"
+        buildcmd += " && git -C " + builddir + " branch --no-color --contains " + job.build_tag + " | grep '\<" + job.default_branch + "\>'"
         buildcmd += " && git -C " + builddir + " reset --hard " + job.build_tag + " --"
         buildcmd += " && git -C " + builddir + " submodule update --init --force --recursive"
         subprocess.run(buildcmd, shell=True, check=True)
@@ -137,7 +127,7 @@ class Builder:
                 _, raw, _ = sshClient.exec_command("(" + self.environment + "source /etc/profile; cd " + builddir
                         + "; make -j`getconf _NPROCESSORS_ONLN` && rm -fr .install && PREFIX=.install make install)  2>&1")
             job.status = raw.channel.recv_exit_status()
-            job.output = raw.read().decode('latin-1') # utf-8 is rejected by Mysql despite it is properly configured
+            job.output = raw.read().decode('latin-1') # FIXME utf-8 is rejected by Mysql despite it is properly configured
 
     def store(self, job):
         logging.info("[" + self.name + "] Storing " + job.build_tag + " from " + job.repository_url + "...")
@@ -169,8 +159,6 @@ class Builder:
                     fname_abs = os.path.join(rdir_abs, fname)
                     rdir_rel = os.path.relpath(rdir_abs, basedir)
                     fname_rel = os.path.join(rdir_rel, fname)
-#                    print("rdir_abs ", rdir_abs, "fname", fname, "fname_abs", fname_abs,
-#                            "rdir_rel", rdir_rel, "fname_rel", fname_rel)
                     if not os.path.islink(fname_abs):
                         with open(fname_abs,"rb") as fd:
                                 bytes = fd.read()
@@ -246,66 +234,6 @@ def reconcile():
             for b in oldbuilder:
                 b.queue.put(Die())
                 b.process.join()
-# TODO build missing tags
-        # Build missing tags
-#        for repo in session.query(db.Repositories).join(db.Providers). \
-#                filter(db.Repositories.name == "cs/ds/fake"). \
-#                with_entities(db.Repositories.id, db.Repositories.name, 
-#                        db.Repositories.type, db.Repositories.platform_id, db.Providers.url).all():
-#            req = requests.get('https://gitlab.elettra.eu/api/v4/projects/' 
-#                    + urllib.parse.quote(repo.name, safe='') + '/repository/tags')
-#            data = req.json()
-#            if req.status_code != 200:
-#                logger.error('Error looking for repository ' + repo.name);
-#            else:
-#                # Retrieve committed tags
-#                ctags = []
-#                for tag in data:
-#                    if tag['target'] != tag['commit']['id']:
-#                        ctags.append(tag['name'])
-#                ctags.sort(key=StrictVersion)
-#                print(ctags)
-#
-#                # Retrieve built tags
-#                btags = []
-#                for build in session.query(db.Builds).filter(db.Builds.repository_id==repo.id).all():
-#                    btags.append(build.tag)
-#                btags.sort(key=StrictVersion)
-#                print(btags)
-#
-#                # Calculate missing tags
-#                mtags = list(set(ctags).difference(set(btags)))
-#                mtags.sort(key=StrictVersion)
-#                print(mtags)
-#                        
-#                if mtags:
-#                    i = ctags.index(mtags[0])
-#                    if i:
-#                        # Re-build the previous built version
-#                        idx = allbuilders[repo.platform_id].index(min(allbuilders[repo.platform_id], 
-#                            key=lambda x:x.queue.qsize()))
-#                        allbuilders[repo.platform_id][idx].queue.put(Job(type=JobType.build, 
-#                            repository_name = repo.name, repository_url = repo.url+ "/" + repo.name + ".git",
-#                            repository_type = repo.type, 
-#                            build_tag='refs/tags/' + ctags[i-1]))
-#
-#                    # Build missing tags
-#                    for mtag in mtags:
-#                        emails = []
-#                        for tag in data:
-#                            if tag['name'] == mtag:
-#                                emails = [tag['commit']['author_email']]
-#                                break
-#                        build = db.Builds(repository_id=repo.id, platform_id=repo.platform_id, tag=mtag)
-#                        session.add(build)
-#                        session.commit()
-#
-#                        idx = allbuilders[repo.platform_id].index(min(allbuilders[repo.platform_id], 
-#                            key=lambda x:x.queue.qsize()))
-#                        allbuilders[repo.platform_id][idx].queue.put(Job(type=JobType.build, 
-#                            repository_name = repo.name, repository_url = repo.url+ "/" + repo.name + ".git",
-#                            repository_type = repo.type, build_tag='refs/tags/' + mtag, 
-#                            build_id = build.id, emails = emails))
     except Exception as e:
         sendEmailAdmins("Reconcilation failed", str(e))
         logger.error("Reconciliation failed: ", str(e))
@@ -328,6 +256,7 @@ class Server(BaseHTTPRequestHandler):
 
             post_json = json.loads(post_data.decode('utf-8'))
             logger.debug(post_json)
+            print(post_json)
 
             # Tag deletion
             if post_json['after'] == '0000000000000000000000000000000000000000':
@@ -345,7 +274,8 @@ class Server(BaseHTTPRequestHandler):
                 if self.headers['X-Gitlab-Event'] == 'Tag Push Hook' and post_json['event_name'] == 'tag_push' and r.enabled:
                     job = Store(repository_name = r.name, repository_url = post_json['project']['ssh_url'], build_tag=post_json['ref'],
                             repository_id = r.id, repository_type = r.type, emails=[post_json['commits'][0]['author']['email'], 
-                                post_json['user_username'] + '@elettra.eu', post_json['user_email']])
+                                post_json['user_username'] + '@elettra.eu', post_json['user_email']],
+                            default_branch=post_json['project']['default_branch'])
                 else:
                     continue
 
@@ -367,13 +297,10 @@ class Server(BaseHTTPRequestHandler):
         finally:
             session.close()
 
-# TODO Migrate to multi-thread HTTP server?
 def run(address, port, server_class=HTTPServer, handler_class=Server):
     logger.info('Starting...')
     server_address = (address, port)
     httpd = server_class(server_address, handler_class)
-#    httpd.socket = ssl.wrap_socket (httpd.socket, keyfile="/etc/ssl/private/inau_elettra_eu.key",
-#            certfile="/etc/ssl/certs/inau_elettra_eu.crt", server_side=True)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -382,10 +309,8 @@ def run(address, port, server_class=HTTPServer, handler_class=Server):
     logger.info('Stopping...')
 
 if __name__ == '__main__':
-    # TODO Migrate to configuration file to avoid hard-coded defaults
-    # Retrieve arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--db", type=str, help='Database URI to connect to', required=True) # TODO Enable password-less access to database
+    parser.add_argument("--db", type=str, help='Database URI to connect to', required=True)
     parser.add_argument('--bind', type=str, default='localhost', help='IP Address or hostname to bind to')
     parser.add_argument('--port', type=int, default=443, help='Port to listen to')
     parser.add_argument("--store", type=str, default='/scratch/build/files-store/', help='Directory where store produced binaries')
